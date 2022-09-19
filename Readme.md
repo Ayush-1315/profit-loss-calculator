6c57fa1c3f0bec92b3402a7d304520a6c3c93517
Ayush Raj <ayush.raj138@gmail.com>
2022-09-20 00:56:14 +0530

    docs: created profit/loss calculator

---
+++ Readme.md
@@ -0,0 +1,8 @@
+#Profit and Loss Calculator
+
+The web application allows you to calculate your profit/loss on a number of stocks you purchased based on the cost prce, net quantity purchased and their current price.
+
+Technologies used:
+1. HTML5
+2. CSS3
+3. JavaScript
---
+++ script.js
@@ -0,0 +1,43 @@
+var costPrice = document.querySelector("#cost-price");
+var stocks = document.querySelector("#stocks");
+var currentPrice = document.querySelector("#current-price");
+var calculate = document.querySelector('input[type="button"]');
+var showAnswer = document.querySelector(".answer");
+calculate.addEventListener("click", calculateProfitAndLoss);
+
+function calculateProfitAndLoss() {
+  let result = 0,
+    cP = parseFloat(costPrice.value),
+    sP = parseFloat(currentPrice.value),
+    resultPercent = 0,
+    quantity = parseInt(stocks.value);
+  if (isNaN(cP)===true || isNaN(sP)===true|| isNaN(quantity)===true) {
+    showAnswer.style.color = "#F13C20";
+    showAnswer.innerHTML = "Please enter proper values";
+  } 
+  else {
+    if (cP > sP) {
+      result = (cP - sP) * quantity;
+      resultPercent = (result / (cP * quantity)) * 100;
+      showAnswer.style.color = "#F13C20";
+      showAnswer.innerHTML =
+        "Oh god 😰! You are at a loss of " +
+        result +
+        " . And Loss percent: " +
+        resultPercent.toFixed(2);
+    } else if (cP < sP) {
+      result = (sP - cP) * quantity;
+      resultPercent = (result / (cP * quantity)) * 100;
+      showAnswer.style.color = "#5CDB95";
+      showAnswer.innerHTML =
+        "Going good 😎! You are at a profit of " +
+        result +
+        " . And Profit percent: " +
+        resultPercent.toFixed(2);
+    } else {
+      showAnswer.style.color = "#379683";
+      showAnswer.innerHTML =
+        "No change in prices...Keep holding your stocks 😕";
+    }
+  }
+}
